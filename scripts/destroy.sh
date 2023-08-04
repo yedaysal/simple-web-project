fd9a6843a4804cdc10d6c8c6d8df9b9728876839
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-08-04 19:29:15 +0300

    Add SSH tunnel destroy command

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -4,16 +4,21 @@
 # - Stops the active minikube cluster
 # - Deletes the stopped minikube cluster
 # - Prunes docker system
+# - Closes the SSH tunnel to the ingress-nginx-controller NodePort service in the minikube container
 # Author: Yunus Emre Daysal
 
-echo "Stopping the active minikube cluster"
+echo "Stopping the active minikube cluster..."
 minikube stop
 echo
 
-echo "Deleting the stopped minikube cluster"
+echo "Deleting the stopped minikube cluster..."
 minikube delete
 echo
 
-echo "Pruning the docker system"
+echo "Pruning the docker system..."
 docker system prune -af
+echo
+
+echo "Closing the SSH tunnel to ingress-nginx-controller..."
+pkill -f "docker@127.0.0.1"
 echo
